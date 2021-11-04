1005673b6269cd6fb2ec8786e8460101cbd857a0
Luchta <peer@lucluc.eu>
2021-11-04 10:54:11 +0100

    cleanup

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -9,7 +9,7 @@
 template <typename T>
 T MessageQueue<T>::receive()
 {
-    //DONE
+    
     // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
     // to wait for and receive new messages and pull them from the queue using move semantics. 
     // The received object should then be returned by the receive function. 
@@ -28,7 +28,7 @@ T MessageQueue<T>::receive()
 template <typename T>
 void MessageQueue<T>::send(T &&msg)
 {
-    //DONE
+    
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
 
@@ -52,7 +52,7 @@ TrafficLight::TrafficLight()
 
 void TrafficLight::waitForGreen()
 {
-    //DONE
+    
     // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
     // runs and repeatedly calls the receive function on the message queue. 
     // Once it receives TrafficLightPhase::green, the method returns.
@@ -76,7 +76,7 @@ TrafficLightPhase TrafficLight::getCurrentPhase()
 
 void TrafficLight::simulate()
 {
-    //DONE
+    
     // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread 
     //when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
     threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
@@ -89,7 +89,7 @@ double TrafficLight::getRandSeconds(){
 // virtual function which is executed in a thread
 void TrafficLight::cycleThroughPhases()
 {
-    //DONE
+    
     // FP.2a : Implement the function with an infinite loop that measures the time between two loop cycles 
     // and toggles the current phase of the traffic light between red and green and sends an update method 
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
